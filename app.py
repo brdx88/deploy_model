25f07308369afbd6c0bd56bced59759e83ecaa04
Brian Ivan Cusuanto <59792214+brdx88@users.noreply.github.com>
2021-09-20 00:43:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from flask.scaffold import _matching_loader_thinks_module_is_package
 from predict import make_predictions
 from xgboost import XGBClassifier
+# from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -12,19 +13,31 @@ def index():
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
-    # input = request.form
+    if request.method == 'POST':
+	    data_input = request.get_json()
+	    data = {}
 
-    # person_age = int(input['person_age'])
-    # person_income = int(input['person_income'])
-    # person_home_ownership = input['person_home_ownership']
-    # person_emp_length = float(input['person_emp_length'])
-    # loan_intent = input['loan_intent']
-    # loan_grade = input['loan_grade']
-    # loan_amnt = int(input['loan_amnt'])
-    # loan_int_rate = float(input['loan_int_rate'])
-    # loan_percent_income = float(input['loan_percent_income'])
-    # cb_person_default_on_file = input['cb_person_default_on_file']
-    # cb_person_cred_hist_length = int(input['cb_person_cred_hist_length'])
+	    data["person_age"] = int(data_input['data']['person_age'])
+	    data["person_income"] = int(data_input['data']['person_income'])
+	    data["person_home_ownership"] = data_input['data']['person_home_ownership']
+	    data["person_emp_length"] = float(data_input['data']['person_emp_length'])
+	    data["loan_intent"] = data_input['data']['loan_intent']
+	    data["loan_grade"] = data_input['data']['loan_grade']
+	    data["loan_amnt"] = int(data_input['data']['loan_amnt'])
+	    data["loan_int_rate"] = float(data_input['data']['loan_int_rate'])
+	    data["loan_percent_income"] = float(data_input['data']['loan_percent_income'])
+	    data["cb_person_default_on_file"] = data_input['data']['cb_person_default_on_file']
+	    data["cb_person_cred_hist_length"] = int(data_input['data']['cb_person_cred_hist_length'])
+
+	    result = make_predictions(data)
+	    result = {
+                    'model':'XGB-Credit-Risk',
+                    "version": '1.0.0',
+                    "prediction": f"{result['data'][0]['proba']} {result['data'][0]['pred']}"
+		  	}
+
+	    return jsonify(result)
+
 
     # data = {
     #     "person_age":person_age,
@@ -52,6 +65,20 @@ def predict():
     }
     print(result)
     return jsonify(result)
+
+@app.route('/predict-api', methods=['POST'])
+def predict_api():
+	data = request.get_json()
+	result = make_predictions(data)
+
+	result = {
+        'model':'XGB-Credit-Risk',
+        "version": '1.0.0',
+        "prediction": f"{result['data'][0]['proba']} {result['data'][0]['pred']}"
+	  	}	
+	print(result)
+	return jsonify(result)
+
 
 
 if __name__ == '__main__':
