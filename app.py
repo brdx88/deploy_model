a311d2a0223e998e576a01694cd8513962f03db0
Brian Ivan Cusuanto <59792214+brdx88@users.noreply.github.com>
2021-09-19 21:04:07 +0700

    add index.html

--- app.py
+++ app.py
@@ -10,8 +10,36 @@ def index():
     # return "Hello, World!"
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
+    # input = request.form
+
+    # person_age = int(input['person_age'])
+    # person_income = int(input['person_income'])
+    # person_home_ownership = input['person_home_ownership']
+    # person_emp_length = float(input['person_emp_length'])
+    # loan_intent = input['loan_intent']
+    # loan_grade = input['loan_grade']
+    # loan_amnt = int(input['loan_amnt'])
+    # loan_int_rate = float(input['loan_int_rate'])
+    # loan_percent_income = float(input['loan_percent_income'])
+    # cb_person_default_on_file = input['cb_person_default_on_file']
+    # cb_person_cred_hist_length = int(input['cb_person_cred_hist_length'])
+
+    # data = {
+    #     "person_age":person_age,
+    #     "person_income":person_income,
+    #     "person_home_ownership":person_home_ownership,
+    #     "person_emp_length":person_emp_length,
+    #     "loan_intent":loan_intent,
+    #     "loan_grade":loan_grade,
+    #     "loan_amnt":loan_amnt,
+    #     "loan_int_rate":loan_int_rate,
+    #     "loan_percent_income":loan_percent_income,
+    #     "cb_person_default_on_file":cb_person_default_on_file,
+    #     "cb_person_cred_hist_length":cb_person_cred_hist_length
+	# }
+
     data = request.get_json()
     result = make_predictions(data)
     result = {
